d1be7593d9ace50299e5bdd96c742dc6f558f4d1
K-DONGMIN <rnlghs4580@naver.com>
2022-11-28 11:37:10 +0900

    Create DeleteRequestDTO

---
+++ src/main/java/com/example/calculator/dto/DeleteRequestDTO.java
@@ -0,0 +1,14 @@
+package com.example.calculator.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class DeleteRequestDTO {
+    private String name;
+}
